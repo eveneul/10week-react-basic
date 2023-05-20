573e137536ffe29838c412c7d0eab9b71107f983
오하늘 <nebulass@naver.com>
2023-05-20 21:52:14 +0900

    proptypes 사용

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,15 @@
 import { memo } from 'react';
+import PropTypes from 'prop-types';
 
 export function Btn(props) {
 	const { text, onClick } = props;
 	console.log(text, 'text~');
 	return <button onClick={onClick}>{text}</button>;
 }
+
+Btn.PropTypes = {
+	text: PropTypes.string,
+	onClick: PropTypes.func,
+};
 
 export const MemoBtn = memo(Btn);
