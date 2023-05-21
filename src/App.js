ea3a475cf6af2452abc75f62fa1cde49b97f1634
오하늘 <nebulass@naver.com>
2023-05-21 18:55:43 +0900

    api 호출해서 화면에 뿌려주기

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import { useEffect, useState } from 'react';
 function App() {
 	const [isLoading, setIsLoading] = useState(true);
 	const [movie, setMovie] = useState([]);
+
+	const getMovies = async () => {
+		const data = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)).json();
+		setMovie(data.data.movies);
+		setIsLoading(false);
+	};
 	useEffect(() => {
-		fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
-			.then((res) => res.json())
-			.then((json) => {
-				setMovie(json.data.movies);
-				setIsLoading(false);
-			});
+		getMovies();
 	}, []);
-	return <div className='App'>{isLoading ? <span>loading...</span> : null}</div>;
+	console.log(movie);
+	return (
+		<div className='App'>
+			{isLoading ? (
+				<span>loading...</span>
+			) : (
+				<div className='container'>
+					{movie.map((item, index) => (
+						<a href='#' key={index}>
+							<img src={item.large_cover_image} alt={item.title} />
+							<br />
+							<strong>{item.title_long}</strong>
+							<p></p>
+						</a>
+					))}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
