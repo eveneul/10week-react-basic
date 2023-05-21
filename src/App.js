b184b311c31cfbdd7867c1c0835b8b43f0cbffaf
오하늘 <nebulass@naver.com>
2023-05-21 18:38:55 +0900

    fetch, then으로 api 불러오기

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  const [coin, setCoin] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((res) => res.json())
-      .then((json) => {
-        setCoin(json);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div className="App">
-      <h1>the coins</h1>
-      {loading ? (
-        <span>Loading..</span>
-      ) : (
-        <ul>
-          {coin.slice(0, 30).map((item) => (
-            <li key={item.id}>
-              <span className="title">{item.name}</span>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+	const [isLoading, setIsLoading] = useState(true);
+	const [movie, setMovie] = useState([]);
+	useEffect(() => {
+		fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
+			.then((res) => res.json())
+			.then((json) => {
+				setMovie(json.data.movies);
+				setIsLoading(false);
+			});
+	}, []);
+	return <div className='App'>{isLoading ? <span>loading...</span> : null}</div>;
 }
 
 export default App;
